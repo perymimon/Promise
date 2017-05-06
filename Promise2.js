01836d32b29985e93b0d0c3169b50cf37b165c13
pery <pery.mimon@gmail.com>
2017-05-06 23:54:38 +0300

    start version 2.0 working progress..

--- Promise2.js
+++ Promise2.js
@@ -2,9 +2,8 @@
     window._Promise = _Promise;
     var PENDING = [/*'pending'*/],
         REJECT = [/*'reject'*/],
-        RESOLVE = [/*'resolve'*/];
-
-
+        RESOLVE = [/*'resolve'*/],
+        FUNCTION = 'function';
 
     function noop (x){}
     function one(func) {
@@ -13,8 +12,6 @@
             func = null;
         }
     }
-
-
 
     function _Promise(cb) {
         var status = PENDING;
@@ -47,8 +44,8 @@
         this.then = function (onFulfilled, onRejected) {
             return new _Promise(function (res, rej) {
                 resolving();
-                resolveQueue.push( then(me, res, rej, onFulfilled, res));
-                rejectQueue.push( then(me, res, rej, onRejected, rej));
+                resolveQueue.push( [me, res, rej, onFulfilled, res] );
+                rejectQueue.push( [me, res, rej, onRejected, rej] );
             })
         };
 
@@ -60,27 +57,26 @@
 
     function _resolving(queue, v, theOtherQueue){
         setTimeout(function () {
-            for (var i = 0, cb; cb = queue[i]; i++) {
-                cb(v);
+            for (var i = 0, args; args = queue[i]; i++) {
+                args.push(v);
+                then.apply(null, args);
             }
             queue.length = theOtherQueue.length = 0;
         }, 0);
     }
-    function then(me, res, rej, on, onAdopt) {
+    function then(me, res, rej, on, pushState, value) {
         var xThen;
 
-        return  function (value){
-            if (typeof on === "function")
-                resolution( on(value) );
-            else
-                onAdopt(value); //adopeState
-        };
+        if (typeof on === FUNCTION)
+            resolution( on(value) );
+        else
+            pushState(value); //adopeState
 
         function resolution(x) {
             var _onFail = rej;
             try {
                 if (x == me) throw TypeError('promise can`t return itself');
-                if (x === Object(x) && typeof (xThen = x.then) === 'function')
+                if (x === Object(x) && typeof (xThen = x.then) === FUNCTION)
                     xThen.call(x, one(function (y) {
                         _onFail = noop;
                         resolution(y)
@@ -95,6 +91,55 @@
             }
         }
 
+    }
+
+    _Promise.all = function(iterable){
+        return new _Promise(function (res, rej) {
+            var ret = [];
+            var count = 0;
+            function ceckout(index,value){
+                ret[index] = value;
+                count ++;
+                if(count == iterable.length)
+                    res(ret)
+            }
+            iterable.forEach(function (promise,i) {
+            })
+        })
+    };
+
+    _Promise.race = function (iterable) {
+        return new _Promise(function (res, rej) {
+            iterable.forEach(function (promise) {
+                promise.then(res, rej)
+            })
+        });
+
+    };
+
+    _Promise.reject = function (reason) {
+        return new _Promise(function (res, rej) {
+            rej(reason)
+        })
+    };
+    _Promise.resolve = function(value){
+        return new _Promise(function (res) {
+            res(value);
+        })
+    };
+
+    _Promise.deferred = function deferred() {
+        var resolved ;
+        var rejected ;
+        var promise = _Promise( function (res, rej) {
+            resolved = res;
+            rejected = rej;
+        });
+        return {
+            promise: promise,
+            resolve: resolved,
+            reject: rejected
+        }
     }
 
 })( typeof exports === 'undefined'? window: module.exports );
