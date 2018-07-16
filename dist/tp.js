0299cb367f6541b1019dd6b8b83425cb8f113e5c
perymimon <pery.mimon@gmail.com>
2018-07-17 00:06:59 +0300

    make little refactor | test pass

--- dist/tp.js
+++ dist/tp.js
@@ -4,134 +4,143 @@
     (global.TP = factory());
 }(this, (function () { 'use strict';
 
-    var PENDING = [/*'pending'*/],
-        REJECT = [/*'reject'*/],
-        RESOLVE = [/*'resolve'*/],
+    var PENDING = ['pending'],
+        REJECT = ['reject'],
+        RESOLVE = ['resolve'],
         FUNCTION = 'function';
 
     function noop(x) {
     }
 
-    function one(func) {
+    function once(func) {
         return function () {
             func && func.apply(this, arguments);
             func = null;
         }
     }
 
+    function dependence(resolve, reject) {
+        return {
+            resolve: function () {
+                resolve && resolve.apply(this, arguments);
+                resolve = reject = null;
+            },
+            reject: function () {
+                reject && reject.apply(this, arguments);
+                resolve = reject = null;
+            }
+        }
+    }
+
     function TP(initializer) {
-        var _status = PENDING;
-        var _value = null; /*also _reason*/
+        var status = PENDING;
+        var value = null;
+        /*also _reason*/
 
-        var _resolveQ = [];
-        var _rejectQ = [];
+        var callbackQueue = [];
         var _resolving = noop;
-        var decidedQueue = null;
 
-        var resolver = one(function(status, value, queue){
-            _status = status;
-            // _reason = reason;
-            _value = value;
-            decidedQueue = queue;
+        var resolver = once(function (_status, _value) {
+            status = _status;
+            value = _value;
             resolving();
             _resolving = resolving;
         });
 
         function fulfilled(value) {
-            resolver(RESOLVE, value, _resolveQ);
+            resolver(RESOLVE, value);
         }
 
         function rejector(reason) {
-            resolver(REJECT, reason, _rejectQ);
+            resolver(REJECT, reason);
         }
-
-
 
         function resolving() {
             setTimeout(function () {
-                var i = 0, res;
-                while (res = decidedQueue[i++]) {
-                    res(_value);
+                var i = 0, dataum;
+                var callbackName = status[0];
+                while (dataum = callbackQueue[i++]) {
+                    then(dataum[0],dataum[1][callbackName]);
+                    // res(_value);
                 }
-                _rejectQ.length = 0;
-                _resolveQ.length = 0;
+                callbackQueue.length = 0;
             }, 0);
         }
 
-        var promisePrototype = {
+        var promise = {
             get status() {
-                return _status[0];
+                return status[0];
             },
             get value() {
-                return _value
+                return value
             },
-            catch:function (onRejected) {
+            catch: function (onRejected) {
                 return TP.then(null, onRejected);
             },
-            then:function(onFulfilled, onRejected) {
-                return new TP(function (resolve, reject) {
-                    _resolving();
-                    _resolveQ.push(then.call(this, resolve, reject, onFulfilled, resolve));
-                    _rejectQ.push(then.call(this, resolve, reject, onRejected , reject));
-                });
+            then: function (onFulfilled, onRejected) {
+                var deferred = TP.deferred();
+                callbackQueue.push([deferred, {resolve:onFulfilled,reject:onRejected } ]);
+                _resolving();
+                return deferred.promise;
             }
         };
 
-
-
-
-
-        function then(resolve, reject, callback, onAdopt) {
-            var me = this;
-            function cb(x){
-                return typeof callback == FUNCTION? resolution(callback(x)): onAdopt(x);
+        // function then(resolve, reject, callback, onAdopt, x) {
+        function then( deferred, callback) {
+            try {
+                typeof callback == FUNCTION ?
+                    resolution(callback(value)):
+                    deferred[status[0]](value);
+            } catch (err) {
+                deferred.reject(err);
             }
-            return function (value) {
-                try {
-                    cb(value);
-                } catch (err) {
-                    reject(err);
-                }
-            };
 
             function resolution(x) {
                 var xThen;
+                var stater = dependence(resolution, deferred.reject);
                 try {
-                    if (x == me) throw TypeError('promise can`t return itself');
+                    if (x === deferred.promise) throw TypeError('promise can`t return itself');
                     if (x === Object(x) && typeof (xThen = x.then) === FUNCTION)
-                        xThen.call(x, one(resolution), reject);
+                        xThen.call(x, stater.resolve, stater.reject);
                     else
-                        resolve(x);
+                        deferred.resolve(x);
 
                 } catch (err) {
-                    reject(err);
+                    stater.reject(err);
                 }
             }
-
         }
 
-
-        var me = Object.create(promisePrototype);
-        me.constructor = initializer;
+        promise.constructor = initializer;
         if (typeof initializer == FUNCTION) {
             try {
-                me.constructor(fulfilled , rejector);
+                promise.constructor(fulfilled, rejector);
             } catch (err) {
                 rejector(err);
             }
         } else {
-            fulfilled (initializer/*value*/);
+            fulfilled(initializer/*value*/);
         }
 
-        return me;
+        return promise;
 
 
     }
 
     TP.all = function (iterable) {
         return new TP(function (res, rej) {
+            var ret = [];
+            var count = 0;
+
+            function checkout(index, value) {
+                ret[index] = value;
+                count++;
+                if (count == iterable.length)
+                    res(ret);
+            }
 
             iterable.forEach(function (promise, i) {
+                promise.then(checkout);
             });
         })
     };
